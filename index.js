2f04c9285e5cefcfc003104adbc938e6a167aa90
KaviArasuNS <KaviArasu_NS@icloud.com>
2022-07-24 20:51:17 +0530

    cors update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from 'cors';
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-// import { usersRouter } from "./routes/users.js";
+
 
 
 
@@ -14,6 +14,7 @@ const PORT= process.env.PORT;
 app.use(express.json());
 
 const MONGO_URL = process.env.MONGO_URL;
+
 async function createConnection(){
   const client = new MongoClient(MONGO_URL);
   await client.connect();
